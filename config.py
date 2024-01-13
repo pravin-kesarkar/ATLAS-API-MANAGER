c35a76b270faed7f38da4b3620f4d63986af78ad
pravin-kesarkar <pwinwork2906@gmail.com>
2024-01-13 23:44:31 +0530

    add atlas codde

---
+++ config.py
@@ -0,0 +1,12 @@
+atlas_db_cread={
+    'host':'atlas-db-uat.c9v4py5vubyn.ap-south-1.rds.amazonaws.com',
+    'user':'master',
+    'password':'AtlasDB123##',
+    'port':3306
+}
+
+open_search_cread={
+    "opensearch_endpoint" : "https://vpc-atlas-uat-domain-2qgs5mabkjzt7e7poczncx4o6y.ap-south-1.es.amazonaws.com:443",
+    "opensearch_user" : "AtlasSearch",
+    "opensearch_password" : "Atlas123##" 
+    }
